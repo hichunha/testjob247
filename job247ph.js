3cb58ab2122e6cb7a0a7ccf6bc946fb269ca7377
Công Hoàng <buihoang3002@gmail.com>
2024-09-19 10:42:34 +0700

    first commit

---
+++ job247ph.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Thiết lập EJS làm template engine
+app.set('view engine', 'ejs');
+
+// Định nghĩa đường dẫn cho các file EJS
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'Public')));
+
+// Tạo route để render file EJS
+app.get('/', (req, res) => {
+    res.render('create_job');
+});
+
+
+// Chạy server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
